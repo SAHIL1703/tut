f16f228bb434b7eef3a8b3c643baaf805720732c
SAHIL1703 <sahilborkar1703@gmail.com>
2022-07-16 23:22:21 +0530

    first commit

---
+++ build/precache-manifest.b7efa8ba55e7e814084bb453dbee740e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f89e6e319ad2e4bc653bc5369ffb9aa",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f9da775e41ab7372cddc",
+    "url": "/my-app/static/js/2.398dbefe.chunk.js"
+  },
+  {
+    "revision": "99bd7b40b465e0818ad6",
+    "url": "/my-app/static/js/main.80d77c02.chunk.js"
+  },
+  {
+    "revision": "2b041497e86eb44edbc6",
+    "url": "/my-app/static/js/runtime-main.b280d19a.js"
+  }
+]);
